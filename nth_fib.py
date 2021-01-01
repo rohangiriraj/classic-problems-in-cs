fb169ce2683ec0cfdf770c87fb9ad6abca12c1af
rohangiriraj <rohangiriraj@gmail.com>
2021-01-01 15:50:03 +0530

    Small bugfix and fixed typo

--- nth_fib.py
+++ nth_fib.py
@@ -4,21 +4,23 @@
 
 
 def nthfib(n):
-    if n == 1:
-        print(f"The {n}st Fibonacci number is 0")
+    if n == 0:
+        print(f"The {n}th Fibonacci number is 0")
+    elif n == 1:
+        print(f"The {n}st Fibonacci number is 1")
     elif n == 2:
         print(f"The {n}nd Fibonacci number is 1")
     else:
         a = 0
         b = 1
-        c = 1
+        c = 0
         counter = 2
-        while counter < n:
+        while counter <= n:
+            c = a + b
             a = b
             b = c
-            c = a + b
             counter += 1
-        print(f"The {n}th Fibonacci number is {c}")
+        print(f"The {n} [th] Fibonacci number is {c}")
 
 
 n = int(input("Enter the value of n \n"))
